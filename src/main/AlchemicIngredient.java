a5993d64ef0d65e6df03ec6cd710eb81b19fc7c1
Quinten Steeland <quinten.steeland@student.kuleuven.be>
2022-05-09 16:40:01 +0200

    Add attributes of AlchemicIngredient

--- src/main/AlchemicIngredient.java
+++ src/main/AlchemicIngredient.java
@@ -1,2 +1,17 @@
 public class AlchemicIngredient {
+
+    Name name;
+    Quantity quantity;
+    IngredientType ingredientType;
+    State state;
+    Temperature temperature;
+
+    public AlchemicIngredient(Quantity quantity) {
+        //bepaalde hoeveelheid stan-
+        //daardingrediënt te maken zonder verdere argumenten, hetgeen ’Water’ zal zijn,
+        //met standaardtemperatuur [0,20].
+        this.temperature = new Temperature(0, 0);
+        this.state=State.LIQUID;
+    }
+
 }
