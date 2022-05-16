61f773e03ed1cf184633e4c293f3f00cf7bbfd6b
Quinten Steeland <quinten.steeland@student.kuleuven.be>
2022-05-16 17:09:23 +0200

    Add IngredientContainer class

---
+++ src/main/IngredientContainer.java
@@ -0,0 +1,15 @@
+/**
+ * A class for ingredient containers.
+ *
+ * @author  Robbe Vanslambrouck
+ * @author  Quinten Steeland
+ * @author  Wout Voet
+ */
+public class IngredientContainer {
+
+    //capacity
+    //usedCapacity
+    /** The ingredient stored in the container. */
+    AlchemicIngredient storedIngredient;
+
+}
