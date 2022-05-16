ade53997c8d2be72ce7395712b6df0c8ce6bcc04
Quinten Steeland <quinten.steeland@student.kuleuven.be>
2022-05-16 17:08:47 +0200

    Add RecipeBook class

---
+++ src/main/RecipeBook.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+/**
+ * A class for a collection of recipes: a recipe book.
+ *
+ * @author  Robbe Vanslambrouck
+ * @author  Quinten Steeland
+ * @author  Wout Voet
+ */
+public class RecipeBook {
+
+    /** A list of recipes. */
+    List<Recipe> recipes;
+
+
+    /**
+     * Function that adds a recipe to the end of the recipe book.
+     * @param   recipe
+     *          The recipe to add.
+     */
+    public void addRecipe(Recipe recipe) {
+        this.recipes.add(recipe);
+    }
+
+    /**
+     * Function that removes a recipe from the recipe book.
+     * @param   recipe
+     *          The recipe to remove.
+     */
+    public void removeRecipe(Recipe recipe) {
+        this.recipes.remove(recipe);
+    }
+
+}
