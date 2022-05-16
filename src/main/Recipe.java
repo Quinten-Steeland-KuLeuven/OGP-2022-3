bbaa751aa098674cb3cc499ede802b5ad29bf02f
Quinten Steeland <quinten.steeland@student.kuleuven.be>
2022-05-16 17:08:57 +0200

    Add Recipe class

---
+++ src/main/Recipe.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+/**
+ * A class for recipes.
+ *
+ * @author  Robbe Vanslambrouck
+ * @author  Quinten Steeland
+ * @author  Wout Voet
+ */
+public class Recipe {
+
+    /** Name of the recipe. */
+    Name name;
+
+    /** A list of steps for the recipe. */
+    List<String> steps;
+
+    /** A list of ingredients for the recipe. */
+    List<AlchemicIngredient> ingredients;
+
+
+
+}
